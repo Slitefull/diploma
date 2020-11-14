fe7b46d76960b9e6fcad8d1d4e6bd96feff8aa59
Slitefull <viptitechko1@gmail.com>
2020-11-14 21:30:51 +0200

    back/Added /auth route (registration, login) + initialize app

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('config')
+
+const app = express()
+
+app.use('/api/auth', require('./routes/auth.routes'))
+
+const PORT = process.env.PORT || config.get('port')
+
+async function start() {
+  try {
+    await mongoose.connect(config.get('diplomaDB'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    })
+    app.listen(PORT, () => {
+      console.log(`Server has been started on port ${PORT}`)
+    })
+  } catch (e) {
+    console.log(e.message)
+  }
+}
+
+start()
---
+++ routes/auth.routes.js
@@ -0,0 +1,53 @@
+const { Router } = require('express')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const Admin = require('../models/admin')
+const config = require('../config/default.json')
+
+
+const router = Router()
+
+router.post('/register', async (req, res) => {
+  try {
+    const { email, password } = req.body
+
+    const candidate = await Admin.findOne()
+    if (candidate) return res.status(400).json({ message: "This user already exists!" })
+
+    const hashedPassword = await bcrypt.hash(password, 12)
+    const admin = new Admin({email, password: hashedPassword})
+    await admin.save
+
+    res.status(201).json({ message: "New user has been created!" })
+  } catch (e) {
+    res.status(500).json({
+      message: "Something went wrong, please try again later."
+    })
+  }
+})
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body
+
+    const admin = Admin.findOne(email)
+    if (!admin) return res.status(400).json({ message: "User is not found." })
+
+    const isMatch = await bcrypt.compare(password, admin.password)
+    if (!isMatch) return res.status(400).json({ message: "Invalid password, please try again" })
+
+    const token = jwt.sign(
+      { userId: admin.id },
+      config.get('jwtSecret'),
+      { expiresIn: '1h' }
+    )
+
+    res.json({ token, userId: user.id })
+  } catch (e) {
+    res.status(500).json({
+      message: "Something went wrong, please try again later."
+    })
+  }
+})
+
+module.exports = router
